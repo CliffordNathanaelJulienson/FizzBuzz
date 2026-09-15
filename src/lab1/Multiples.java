eb7baa33b9537b2ec4c0882652a3286d6032310b
CliffordNathanaelJulienson <cliffordjulienson123@gmail.com>
2026-09-15 19:35:11 -0400

    Extra Exercise

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,20 @@
+package lab1;
+
+public class Multiples {
+   public static void main() {
+       int counter = multiples(1000, 3, 5);
+       System.out.println(counter);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int counter = 0;
+        int i = 1;
+        while (i < n){
+            if (i % a == 0 || i % b ==0){
+                counter++;
+            }
+            i++;
+        }
+        return counter;
+    }
+}
